0b74191d4a4899a3d82475637e0607df5e3734ae
andreztxt <animaspky@gmail.com>
2025-03-05 14:05:01 -0500

    Refactor Menu class to improve naming conventions and add main, login, and register menu functionalities

--- Gestion_Paquetes/views.py
+++ Gestion_Paquetes/views.py
@@ -1,25 +1,52 @@
 class Menu:
-    def _init_(self, nombre):
-        self.nombre = nombre
-        self.opciones = {}
+    def _init_(self, name):
+        self.name = name
+        self.options = {}
 
-    def agregar_opcion(self, clave, descripcion, funcion):
-        self.opciones[clave] = (descripcion, funcion)
+    def add_option(self, clave, description, funct):
+        self.options[clave] = (description, funct)
 
-    def mostrar(self):
-        print(f"\n--- {self.nombre} ---")
-        for clave, (descripcion, _) in self.opciones.items():
-            print(f"{clave}. {descripcion}")
-            
+    def show(self):
+        print(f"\n--- {self.name} ---")
+        for clave, (description, _) in self.options.items():
+            print(f"{clave}. {description}")
+
     def ejecutar(self):
         while True:
-            self.mostrar()
-            opcion = input("Seleccione una opción (o 'q' para salir): ")
-            if opcion == 'q':
+            self.show()
+            option = input("Seleccione una opción (o 'q' para salir): ")
+            if option == 'q':
                 print("Saliendo del menú...")
                 break
-            if opcion in self.opciones:
-                descripcion, funcion = self.opciones[opcion]
-                funcion()
+            if option in self.options:
+                description, funct = self.options[option]
+                funct()
             else:
                 print("Opción no válida. Intente de nuevo.")
+
+def MainMenu(Menu):
+    def __init__(self):
+        super()._init_("Menú Principal")
+        self.add_option("1", "Loguearse", self.login)
+        self.add_option("2", "Registrarse", self.register)
+        self.add_option("q", "Salir", self.leave)
+
+    def login(self):
+        print("Logueando...")
+        LoginMenu().ejecutar()
+
+    def register(self):
+        print("Registrando...")
+        RegisterMenu().ejecutar()
+
+def LoginMenu(Menu):
+    def __init__(self):
+        super()._init_("Menú de Logueo")
+        self.add_option("1", "Ingresar usuario", self.username)
+        self.add_option("2", "Ingresar contraseña", self.password)
+
+def RegisterMenu(Menu):
+    def __init__(self):
+        super()._init_("Menú de Registro")
+        self.add_option("1", "Ingresar usuario", self.username)
+        self.add_option("2", "Ingresar contraseña", self.password)
