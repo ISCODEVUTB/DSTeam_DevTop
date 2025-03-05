a8aace578332944c9655c6c13775e7b4542c04d9
Oyhs-co <oyhsotelo@gmail.com>
2025-03-04 22:41:46 -0500

    `Se agrega models.py con las clases Paquete, Usuario, Envio and GestionPagos y se remueve la clase Paquete de main.py`

---
+++ Gestion_Paquetes/models.py
@@ -0,0 +1,25 @@
+class Paquete:
+    def __init__(self, nombre, peso, destino):
+        self.nombre = nombre
+        self.peso = peso
+        self.destino = destino
+
+
+class Usuario:
+    def __init__(self, nombre, correo, contraseña, direccion):
+        self.nombre = nombre
+        self.correo = correo
+        self.contraseña = contraseña
+        self.direccion = direccion
+
+class Envio:
+    def __init__(self, usuario, paquete, costo, fecha):
+        self.usuario = usuario
+        self.paquete = paquete
+        self.costo = costo
+        self.fecha = fecha
+
+class GestionPagos:
+    def __init__(self, envio, monto):
+        self.envio = envio
+        self.monto = monto
