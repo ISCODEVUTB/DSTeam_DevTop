7858280b96bfe38f71bb79023691bc08c6a0cdf0
Oyhs-co <oyhsotelo@gmail.com>
2025-03-05 23:27:20 -0500

    `Added pandas and os imports, modified Manager class to include data management functionality`

--- Gestion_Paquetes/controllers.py
+++ Gestion_Paquetes/controllers.py
@@ -1,22 +1,40 @@
+import pandas as pd
+import os
 class Manager:
-    def __init__(self, name, n_elements):
+    def __init__(self, name):
         self.name = name
-        self.n_elements = n_elements
+        self.n_elements = None
+        self.data = None
 
     def Data(self):
-        pass 
+        self.path = "./data/" + self.name + ".csv"   
+        df = pd.read_csv(self.path) 
+        self.data = df
     
-    def AddRecord(self):
-        pass
+    def AddRecord(self, record):
+        NewRecord = pd.DataFrame([[record.values()]], columns = record.keys())
+        self.data = pd.concat([self.data, NewRecord], ignore_index=True)
+        self.data.to_csv(self.path, index=False)
+        print("Datos agregados con exito")
 
-    def EditRecord(self):
-        pass
+    def EditRecord(self,record):
+        SearchID = record['ID']
+        for SearchID in self.data["ID"].values:
+            self.data.loc[self.data["ID"] == SearchID] = record
+            self.data.to_csv(self.path, index=False)
+        else:
+            print("No se encontro el registro a editar")
 
-    def DeletedRecord(self):
-        pass
+    def DeletedRecord(self,record):
+        SearchID = record['ID']
+        for SearchID in self.data["ID"].values:
+            self.data.drop(self.data[self.data["ID"] == SearchID].index, inplace=True)
+            self.data.to_csv(self.path, index=False)
+        else:
+            print("No se encontro el registro a eliminar")
 
     def Show(self):
-        pass
+        print(self.data)
 
 class LoginManager(Manager):
     def __init__(self, username, password):
